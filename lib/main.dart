41c06fa8ab40afda11979672916f5acc832baa22
davisantosr <davi-rocha2@hotmail.com>
2021-03-07 10:49:31 -0300

    using StatefulWidget | fixing decorator position

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 
 main() => runApp(new AskApp());
 
-@override
-class AskApp extends StatelessWidget {
+class AskAppState extends State<AskApp> {
+  var selectedAsk = 0;
+
   void answer() {
+    setState(() {
+      selectedAsk++;
+    });
     print('Answered');
   }
 
+  @override
   Widget build(BuildContext context) {
     final List<String> asks = [
       'What is your favorite color?',
@@ -20,25 +25,21 @@ class AskApp extends StatelessWidget {
           title: Text('Asking'),
         ),
         body: Column(children: [
-          Text(asks.elementAt(0)),
+          Text(asks.elementAt(selectedAsk)),
           ElevatedButton(
             child: Text('Choice 1'),
             onPressed: answer,
           ),
-          ElevatedButton(
-            child: Column(
-              children: [Text('Test 1'), Text('Test2')],
-            ),
-            onPressed: () {
-              print('Answer 2');
-            },
-          ),
-          ElevatedButton(
-            child: Text('Choice 3'),
-            onPressed: () => print('Answer 3'),
-          ),
+          ElevatedButton(child: Text('Choice 2'), onPressed: answer),
+          ElevatedButton(child: Text('Choice 3'), onPressed: answer),
         ]),
       ),
     );
+  }
+}
+
+class AskApp extends StatefulWidget {
+  AskAppState createState() {
+    return AskAppState();
   }
 }
