dc8f133fbd7fa2f37383471776b99098631264f1
davisantosr <davi-rocha2@hotmail.com>
2021-03-07 09:48:07 -0300

    widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new AskApp());
-}
+main() => runApp(new AskApp());
 
+@override
 class AskApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Asking'),
+        ),
+        body: Text('Hello'),
+      ),
     );
   }
 }
