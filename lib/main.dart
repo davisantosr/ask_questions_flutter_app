ecf921e486dc1d769b822f3adad67fe1dedf385f
davisantosr <davi-rocha2@hotmail.com>
2021-03-07 10:04:40 -0300

    using Column

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,31 @@ main() => runApp(new AskApp());
 @override
 class AskApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    final List<String> asks = [
+      'What is your favorite color?',
+      'What is your favorite pet?'
+    ];
+
     return new MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Asking'),
         ),
-        body: Text('Hello'),
+        body: Column(children: [
+          Text(asks.elementAt(0)),
+          ElevatedButton(
+            child: Text('Choice 1'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Choice 2'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Choice 3'),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }
